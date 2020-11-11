fff2d02127e8bf778d80ecc661a017ba9e7632ef
antocecere <antocecere77@gmail.com>
2020-11-11 07:00:16 +0100

    Resource server project creation

---
+++ ResourceServer/src/main/java/com/antocecere77/ws/api/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.antocecere77.ws.api.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
---
+++ ResourceServer/src/test/java/com/antocecere77/ws/api/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antocecere77.ws.api.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
