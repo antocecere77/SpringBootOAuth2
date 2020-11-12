d11778b2c9cc162d0cbf202d86c2d092e99b2eb1
antocecere <antocecere77@gmail.com>
2020-11-12 07:26:03 +0100

    Override the default httpsecurity configuration

--- ResourceServer/src/main/java/com/antocecere77/ws/api/resourceserver/security/WebSecurity.java
+++ ResourceServer/src/main/java/com/antocecere77/ws/api/resourceserver/security/WebSecurity.java
@@ -1,10 +1,18 @@
 package com.antocecere77.ws.api.resourceserver.security;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                   .anyRequest().authenticated()
+                   .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
 }
