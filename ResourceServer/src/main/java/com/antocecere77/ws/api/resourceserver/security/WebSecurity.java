d4acbb5a63a6eb2a6a2f15f25d51dd0d2921102a
antocecere <antocecere77@gmail.com>
2020-11-12 07:20:22 +0100

    Create WebSecurityConfigurerAdapter and enable web security

---
+++ ResourceServer/src/main/java/com/antocecere77/ws/api/resourceserver/security/WebSecurity.java
@@ -0,0 +1,10 @@
+package com.antocecere77.ws.api.resourceserver.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+
+}
