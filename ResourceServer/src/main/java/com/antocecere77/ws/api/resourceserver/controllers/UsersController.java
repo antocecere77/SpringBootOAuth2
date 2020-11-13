7a9dd3576c5613dd3bacd5571f78db9e11dff9c6
antocecere <antocecere77@gmail.com>
2020-11-13 07:37:21 +0100

    @PreAuthorized annotation

--- ResourceServer/src/main/java/com/antocecere77/ws/api/resourceserver/controllers/UsersController.java
+++ ResourceServer/src/main/java/com/antocecere77/ws/api/resourceserver/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package com.antocecere77.ws.api.resourceserver.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +14,9 @@ public class UsersController {
         return "Working...";
     }
 
-    @Secured("ROLE_developer")
+    @PreAuthorize("hasAuthority('ROLE_developer')")
+    //@PreAuthorize("hasRole('developer')")
+    //@Secured("ROLE_developer")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id) {
         return "Delete user with id " + id;
