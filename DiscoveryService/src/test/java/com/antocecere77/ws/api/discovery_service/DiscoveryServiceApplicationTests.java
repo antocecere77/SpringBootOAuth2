0e797d1c5662fc6f1d7a3a653739add434416be3
antocecere <antocecere77@gmail.com>
2020-11-16 07:40:39 +0100

    Creating eureka discovery service project

---
+++ DiscoveryService/src/test/java/com/antocecere77/ws/api/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antocecere77.ws.api.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
