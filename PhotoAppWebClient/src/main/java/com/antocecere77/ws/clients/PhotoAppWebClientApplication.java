6018d751499ac68d9dc2e9ea376547493ae99af9
antocecere <antocecere77@gmail.com>
2020-11-17 07:51:36 +0100

    Creating a new spring boot project

---
+++ PhotoAppWebClient/src/main/java/com/antocecere77/ws/clients/PhotoAppWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.antocecere77.ws.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppWebClientApplication.class, args);
+	}
+
+}
