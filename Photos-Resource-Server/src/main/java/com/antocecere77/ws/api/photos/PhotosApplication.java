6e8c512da1482acfb2bcf439335d17060559429a
antocecere <antocecere77@gmail.com>
2020-11-17 06:59:29 +0100

    @EnableDiscoveryClient for photo and album microservice

--- Photos-Resource-Server/src/main/java/com/antocecere77/ws/api/photos/PhotosApplication.java
+++ Photos-Resource-Server/src/main/java/com/antocecere77/ws/api/photos/PhotosApplication.java
@@ -2,7 +2,9 @@ package com.antocecere77.ws.api.photos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotosApplication {
 
