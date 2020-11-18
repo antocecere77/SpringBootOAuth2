32e5345f6e386aedf9576ae1900067f78e9948f5
antocecere <antocecere77@gmail.com>
2020-11-18 07:26:56 +0100

    Reading id token

--- PhotoAppWebClient/src/main/java/com/antocecere77/ws/clients/photoappwebclient/controllers/AlbumsController.java
+++ PhotoAppWebClient/src/main/java/com/antocecere77/ws/clients/photoappwebclient/controllers/AlbumsController.java
@@ -1,18 +1,25 @@
 package com.antocecere77.ws.clients.photoappwebclient.controllers;
 
 import com.antocecere77.ws.clients.photoappwebclient.photoappwebclient.response.AlbumRest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class AlbumsController {
 
     @GetMapping("/albums")
-    public String getAlbums(Model model) {
+    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+
+        System.out.println("Principal = " + principal);
+        OidcIdToken idToken = principal.getIdToken();
+        String idTokenValue = idToken.getTokenValue();
+        System.out.println("idTokenValue = " + idTokenValue);
 
         AlbumRest album = AlbumRest.builder()
                 .albumId("albumOne")
