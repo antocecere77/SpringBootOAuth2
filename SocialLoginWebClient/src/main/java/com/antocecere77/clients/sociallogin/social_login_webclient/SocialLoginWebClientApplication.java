eaed9cca303798fc00f4ff71cbec5551b3961b75
antocecere <antocecere77@gmail.com>
2020-11-19 07:45:15 +0100

    Creating a new project

---
+++ SocialLoginWebClient/src/main/java/com/antocecere77/clients/sociallogin/social_login_webclient/SocialLoginWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.antocecere77.clients.sociallogin.social_login_webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginWebClientApplication.class, args);
+	}
+
+}
---
+++ SocialLoginWebClient/src/test/java/com/antocecere77/clients/sociallogin/social_login_webclient/SocialLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antocecere77.clients.sociallogin.social_login_webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
