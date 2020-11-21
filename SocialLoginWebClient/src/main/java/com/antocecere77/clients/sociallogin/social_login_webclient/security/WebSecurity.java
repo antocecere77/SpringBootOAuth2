5dd709139cc96d2caf4322a380dfb58302afa93a
antocecere <antocecere77@gmail.com>
2020-11-21 12:10:22 +0100

    implementing oidcClientInitiatedLogoutSuccessHandler

--- SocialLoginWebClient/src/main/java/com/antocecere77/clients/sociallogin/social_login_webclient/security/WebSecurity.java
+++ SocialLoginWebClient/src/main/java/com/antocecere77/clients/sociallogin/social_login_webclient/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.antocecere77.clients.sociallogin.social_login_webclient.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,9 +22,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .oauth2Login()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/")
+                //.logoutSuccessUrl("/")
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .deleteCookies("JSESSIONID");
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+        return successHandler;
     }
 }
